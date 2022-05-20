4930b7b6c51bbea11de1eb4a12db162908c409a2
dungbik <q1w2e30630@gmail.com>
2022-05-20 20:12:32 +0900

    Feat: Add Judge ResultCode

--- src/main/java/yooneeverse/Constants.java
+++ src/main/java/yooneeverse/Constants.java
@@ -3,4 +3,19 @@ package yooneeverse;
 public class Constants {
     public static final String JUDGER_PATH = "/usr/lib/judger/libjudger.so";
 
+    enum ResultCode {
+        WRONG_ANSWER(-1),
+        SUCCESS(0),
+        CPU_TIME_LIMIT_EXCEEDED(1),
+        REAL_TIME_LIMIT_EXCEEDED(2),
+        MEMORY_LIMIT_EXCEEDED(3),
+        RUNTIME_ERROR(4),
+        SYSTEM_ERROR(5)
+        ;
+        private int value;
+
+        ResultCode(int value) {
+            this.value = value;
+        }
+    }
 }
